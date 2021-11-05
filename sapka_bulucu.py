fd653c47ebd8ee87f096ae55b3582f24251bb096
Burak Alanyalıoğlu <balanya25@gmail.com>
2021-11-06 01:32:58 +0300

    Update sapka_bulucu.py

--- sapka_bulucu.py
+++ sapka_bulucu.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Nov  4 23:32:46 2021
-
-@author: HP PC
+Booldum App
+@author: Burak Alanyalıoğlu
 """
 wrong_list_word = []
 correct_list_word = []
 soru = ""
-
 
 file1 = open("list_of_words.txt", "r")
 file2 = open("created_text.txt", "w")
@@ -19,19 +18,14 @@ def position_of_word(text, word):
     new_text = text[pos_word : pos_word + length_of_word + 8]
     return new_text
     
-
-
-
 for lines in file3:
     lines1 = lines[:-1]
     wrong_list_word.append(lines1)
-    
-    
+       
 for new_lines in file1:
     new_lines1 = new_lines[:-1]
     correct_list_word.append(new_lines1) 
-    
-    
+       
 text = input("Lütfen metninizi girin: ")
 
 for i in range (len(wrong_list_word)):
@@ -42,9 +36,6 @@ for i in range (len(wrong_list_word)):
         if soru == "h":
             text = text.replace(wrong_list_word[i], wrong_list_word[i])
         
-            
-
-
 file2.write(text)
 print(text)
 
